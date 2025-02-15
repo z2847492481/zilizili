4318bed0793acb17f8abec1f42c94c127bece33a
zhqlxl <z465719151@outlook.com>
2025-02-16 00:45:47 +0800

    导航栏雏形2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'normalize.css'
+import '@/assets/css/reset.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
